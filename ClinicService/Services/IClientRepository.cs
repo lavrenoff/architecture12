606537d6c29235c89a90c47f47470285f8115325
Лавренов Евгений <lavr020775@gmail.com>
2024-02-09 14:27:47 +0300

    init

---
+++ ClinicService/Services/IClientRepository.cs
@@ -0,0 +1,12 @@
+﻿using ClinicService.Models;
+
+namespace ClinicService.Services
+{
+    /// <summary>
+    /// Конкретный тип интерфейса репозитория для работы со справочником клиентов в БД
+    /// </summary>
+    public interface IClientRepository : IRepository<Client, int>
+    {
+
+    }
+}
